a72f3ae593434eab213a5033a2d6d24f955e0872
Ryan Ray <ryan314r@gmail.com>
2019-03-27 09:57:59 -0500

    WIP: Load and run files

--- runners/java.sh
+++ runners/java.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+PATH="$PATH:/usr/lib/jvm/java-1.8-openjdk/bin"
+
 case "$1" in
 	compile)
 		exec javac $2
--- runners/runner.sh
+++ runners/runner.sh
@@ -5,12 +5,26 @@ error() {
 	echo $1 >&2
 }
 
+timedKiller() {
+	sleep 3
+	kill -9 $1
+}
+
+# Run a program for a limited amount of time
+limitTime() {
+	timedKiller $1 &
+	wait $1
+	status=$?
+	kill $!
+	return $status
+}
+
 # Compile and run the code
 run() {
 	local fileName="$1"
 	local lang="java"
 
-	case "${lang##*.}" in
+	case "${fileName##*.}" in
 		java)
 			lang="java"
 			;;
@@ -19,11 +33,17 @@ run() {
 			;;
 	esac
 
-	./$lang.sh compile $fileName || error "Compile error"
-	./$lang.sh run $fileName || error "Runtime error"
+	/app/$lang.sh compile $fileName || {
+		error "Compile error"
+		return
+	}
+
+	/app/$lang.sh run $fileName &
+	limitTime $! || error "Runtime error"
 }
 
-# while true; do
-# 	# TODO: Check for new submissions
-# 	sleep $POLLING_RATE
-# done
+cd /mnt/submissions
+while true; do
+	[ -f "/mnt/submissions/foo.java" ] && run /mnt/submissions/foo.java
+	sleep $POLLING_RATE
+done
