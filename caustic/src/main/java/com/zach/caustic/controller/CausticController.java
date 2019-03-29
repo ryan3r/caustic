fff82cc110bbb9c42d398bd83fe4571642ec6caf
ZachAttackMLR <goforitmlr@gmail.com>
2019-03-29 11:04:50 -0500

    "init commit for Zach"

---
+++ caustic/src/main/java/com/zach/caustic/controller/CausticController.java
@@ -0,0 +1,30 @@
+package main.java.com.zach.caustic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class CausticController {
+    @GetMapping("/profile")
+    public String profile(Model model,
+                          @RequestParam(value="firstName", required=false, defaultValue="Zach") String firstName,
+                          @RequestParam(value="lastName", required=false, defaultValue="Gorman") String lastName,
+                          @RequestParam(value="email", required=false, defaultValue="zgorman2@iastate.edu") String email,
+                          @RequestParam(value="rank", required=false, defaultValue="") int rank) {
+
+        model.addAttribute("firstName", firstName);
+        model.addAttribute("lastName", lastName);
+        model.addAttribute("email", email);
+        model.addAttribute("rank", rank);
+
+        return "profile";
+    }
+
+    @GetMapping("/results")
+    public String results(Model model, @RequestParam(value="result", required=false, defaultValue="fail") String result) {
+        model.addAttribute("result", result);
+        return "results";
+    }
+}
---
+++ caustic/src/main/java/com/zach/caustic/model/User.java
@@ -0,0 +1,17 @@
+package main.java.com.zach.caustic.model;
+
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private int rank;
+    private String email;
+
+    public User(){}
+
+    public User(String firstName, String lastName, String email){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
