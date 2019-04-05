214eddad7c81086be2f83d61ac2dc66edfd0a26d
Ryan Ray <ryan314r@gmail.com>
2019-04-05 00:13:19 -0500

    Add a basic entrypoint

--- runner/main.go
+++ runner/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
-	
+	"os"
+	"io/ioutil"
+	"log"
+	"runner"
+	"fmt"
 )
 
 func main() {
-	
+	os.Chdir("/mnt/submissions")
+
+	files, err := ioutil.ReadDir(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		fmt.Printf("%v: %v\n", file.Name(), runner.Test(file.Name(), "1 2 3"))
+	}
 }
