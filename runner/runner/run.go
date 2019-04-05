fa868927aa29ea349e62528345e4160b6ba727a6
Ryan Ray <ryan314r@gmail.com>
2019-04-04 22:08:28 -0500

    Add run tests

--- runner/runner/run.go
+++ runner/runner/run.go
@@ -7,3 +7,7 @@ import (
 func compile(filename string) error {
 	return exec.Command("javac", filename).Run()
 }
+
+func run(filename string, output chan []byte, errors chan error) {
+
+}
--- runner/runner/run_test.go
+++ runner/runner/run_test.go
@@ -4,6 +4,16 @@ import (
 	"testing"
 )
 
+func TestCanDetectFileType(t *testing.T) {
+	name, ft := detectType("foo.java")
+	if name != "foo" {
+		t.Errorf("Expected name to be foo but got %v", name)
+	}
+	if ft != "java" {
+		t.Errorf("Expected file type to be java but got %v", ft)
+	}
+}
+
 func TestCanCompileJava(t *testing.T) {
 	if err := compile("ok.java"); err != nil {
 		t.Errorf("ok.java failed to compile (should have): %v", err)
@@ -13,5 +23,31 @@ func TestCanCompileJava(t *testing.T) {
 func TestCompileCanHandleErrors(t *testing.T) {
 	if err := compile("error.java"); err == nil {
 		t.Errorf("error.java compiled (should not have): %v", err)
+	}
+}
+
+func TestRunCanGetOutput(t *testing.T) {
+	errors := make(chan error)
+	output := make(chan []byte)
+
+	run("ok.java", output, errors)
+
+	select {
+	case <- output:
+	case err := <- errors:
+		t.Errorf("An error occured while running %v", err)
+	}
+}
+
+func TestRunCanHandleErrors(t *testing.T) {
+	errors := make(chan error)
+	output := make(chan []byte)
+
+	run("invalid.java", output, errors)
+
+	select {
+	case <- output:
+		t.Errorf("No errors occured while running (expected one)")
+	case <- errors:
 	}
 }
