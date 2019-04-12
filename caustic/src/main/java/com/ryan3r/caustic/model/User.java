d4a9c68feef5801be0b9e50b2dec6d05a90546ff
ZachAttackMLR <goforitmlr@gmail.com>
2019-04-12 09:16:57 -0500

    added templating for profile and results in comments, and adjusted user model to have full name rather than first and last

--- caustic/src/main/java/com/ryan3r/caustic/model/User.java
+++ caustic/src/main/java/com/ryan3r/caustic/model/User.java
@@ -2,16 +2,14 @@ package com.ryan3r.caustic.model;
 
 public class User {
 
-    private String firstName;
-    private String lastName;
+    private String name;
     private int rank;
     private String email;
 
     public User(){}
 
-    public User(String firstName, String lastName, String email){
-        this.firstName = firstName;
-        this.lastName = lastName;
+    public User(String name, String email){
+        this.name = name;
         this.email = email;
     }
 }
