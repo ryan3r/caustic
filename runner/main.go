2bcf1a9ab1eaaddd151aeb65976dd72b0337bdfe
Ryan Ray <ryan314r@gmail.com>
2019-04-17 11:35:05 -0500

    WIP: Break up monolithic main

--- runner/main.go
+++ runner/main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"context"
-	"io"
-	"os"
+	"encoding/json"
+	"fmt"
 
 	"github.com/ahmetalpbalkan/dlog"
 
@@ -13,62 +14,128 @@ import (
 	"github.com/docker/docker/client"
 )
 
+type PullProgress struct {
+	Current int64 `json:"current"`
+	Total   int64 `json:"total"`
+}
+
+type PullResponse struct {
+	Status         string       `json:"status"`
+	ProgressDetail PullProgress `json:"progressDetail"`
+	Progress       string       `json:"progress"`
+	ID             string       `json:"id"`
+}
+
 func main() {
+	ctx := context.Background()
 	cli, err := client.NewClientWithOpts(client.WithVersion("1.39"))
 	panicIf(err)
 
-	res, err := cli.ContainerCreate(context.Background(), &container.Config{
-		AttachStdin:  true,
-		AttachStdout: true,
-		Image:        "openjdk:13-jdk-alpine",
-		WorkingDir:   "/mnt",
-		Cmd:          []string{"sleep", "100"},
-	}, &container.HostConfig{
-		Mounts: []mount.Mount{
-			mount.Mount{
-				Type:   mount.TypeVolume,
-				Source: "cas",
-				Target: "/mnt",
-			},
-		},
-	}, nil, "caustic-running")
+	panicIf(pullImage(ctx, cli, "openjdk:13-jdk-alpine"))
+
+	res, err := createContainer(ctx, cli, "openjdk:13-jdk-alpine", false, []string{"javac", "ok.java"})
 	panicIf(err)
 
-	panicIf(cli.ContainerStart(context.Background(), res.ID, types.ContainerStartOptions{}))
-
-	execID, err := cli.ContainerExecCreate(context.Background(), res.ID, types.ExecConfig{
-		Cmd:          []string{"javac", "ok.java"},
-		AttachStdout: true,
-		AttachStderr: true,
-	})
-	panicIf(err)
-
-	con, err := cli.ContainerExecAttach(context.Background(), execID.ID, types.ExecStartCheck{})
-	panicIf(err)
-	defer con.Close()
-
-	io.Copy(os.Stdout, dlog.NewReader(con.Reader))
-
-	for i := 0; i < 3; i++ {
-		execID, err = cli.ContainerExecCreate(context.Background(), res.ID, types.ExecConfig{
-			Cmd:          []string{"java", "ok"},
-			AttachStdout: true,
-		})
+	doneC, errC := cli.ContainerWait(ctx, res.ID, "")
+	select {
+	case err = <-errC:
 		panicIf(err)
-
-		con, err = cli.ContainerExecAttach(context.Background(), execID.ID, types.ExecStartCheck{})
-		panicIf(err)
-		defer con.Close()
-
-		io.Copy(os.Stdout, dlog.NewReader(con.Reader))
+	case <-doneC:
 	}
 
+	res, err = createContainer(ctx, cli, "openjdk:13-jdk-alpine", false, []string{"sleep", "100"})
+	panicIf(err)
+
+	for i := 0; i < 3; i++ {
+		msg, err := execInContainer(ctx, cli, res.ID, []string{"java", "ok"})
+		panicIf(err)
+		fmt.Print(msg)
+	}
+
+	msg, err := execInContainer(ctx, cli, res.ID, []string{"sh", "-c", "exit 1"})
+	panicIf(err)
+	fmt.Print(msg)
+
 	cli.ContainerStop(context.Background(), res.ID, nil)
-	cli.ContainerRemove(context.Background(), res.ID, types.ContainerRemoveOptions{})
 }
 
 func panicIf(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func createContainer(ctx context.Context, cli client.APIClient, image string, readonly bool, cmd []string) (container.ContainerCreateCreatedBody, error) {
+	res, err := cli.ContainerCreate(ctx, &container.Config{
+		AttachStdin:     true,
+		AttachStdout:    true,
+		Image:           image,
+		WorkingDir:      "/mnt",
+		NetworkDisabled: true,
+		Cmd:             cmd,
+	}, &container.HostConfig{
+		ReadonlyRootfs: true,
+		AutoRemove:     true,
+		Mounts: []mount.Mount{
+			mount.Mount{
+				Type:     mount.TypeVolume,
+				Source:   "cas",
+				Target:   "/mnt",
+				ReadOnly: readonly,
+			},
+		},
+	}, nil, "caustic-running")
+	if err != nil {
+		return container.ContainerCreateCreatedBody{}, err
+	}
+
+	return res, cli.ContainerStart(ctx, res.ID, types.ContainerStartOptions{})
+}
+
+func execInContainer(ctx context.Context, cli client.APIClient, containerID string, cmd []string) (string, error) {
+	execID, err := cli.ContainerExecCreate(ctx, containerID, types.ExecConfig{
+		Cmd:          cmd,
+		AttachStdout: true,
+		AttachStderr: true,
+	})
+	if err != nil {
+		return "", err
+	}
+
+	con, err := cli.ContainerExecAttach(ctx, execID.ID, types.ExecStartCheck{})
+	if err != nil {
+		return "", err
+	}
+	defer con.Close()
+
+	buffer := new(bytes.Buffer)
+	buffer.ReadFrom(dlog.NewReader(con.Reader))
+
+	return buffer.String(), nil
+}
+
+func pullImage(ctx context.Context, cli client.APIClient, image string) error {
+	pullStats, err := cli.ImagePull(ctx, image, types.ImagePullOptions{})
+	if err != nil {
+		return err
+	}
+
+	decoder := json.NewDecoder(pullStats)
+	for decoder.More() {
+		var progress PullResponse
+		decoder.Decode(&progress)
+
+		switch progress.Status {
+		case "Downloading":
+			fmt.Printf("Downloading %s\n", progress.Progress)
+
+		case "Extracting":
+			fmt.Printf("Extracting %s\n", progress.Progress)
+
+		default:
+			fmt.Println(progress.Status)
+		}
+	}
+
+	return nil
 }
