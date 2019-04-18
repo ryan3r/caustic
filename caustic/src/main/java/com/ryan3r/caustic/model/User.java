dfde29dbb7ab0bfb3126fe4e389a80b9ad9b97fa
ZachAttackMLR <goforitmlr@gmail.com>
2019-04-18 17:57:05 -0500

    removed error by adding ID to User.java

--- caustic/src/main/java/com/ryan3r/caustic/model/User.java
+++ caustic/src/main/java/com/ryan3r/caustic/model/User.java
@@ -1,10 +1,12 @@
 package com.ryan3r.caustic.model;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class User {
 
+    @Id
     private String name;
     private int rank;
     private String email;
