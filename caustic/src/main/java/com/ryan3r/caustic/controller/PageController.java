4acc28bc022c2dd2ddcbb507a75be7deeac89d58
ZachAttackMLR <goforitmlr@gmail.com>
2019-04-19 01:37:38 -0500

    Changed PageController and templated pages to properly pull from database

--- caustic/src/main/java/com/ryan3r/caustic/controller/PageController.java
+++ caustic/src/main/java/com/ryan3r/caustic/controller/PageController.java
@@ -1,15 +1,14 @@
 package com.ryan3r.caustic.controller;
 
+import com.ryan3r.caustic.model.accounts;
 import com.ryan3r.caustic.repository.accountsRepository;
-import com.ryan3r.caustic.model.Scoreboard;
+import com.ryan3r.caustic.repository.submitsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class PageController {
@@ -17,36 +16,41 @@ public class PageController {
     @Value("${spring.application.name}")
     String appName;
 
+    private final accountsRepository accRep;
+    private final submitsRepository subRep;
+
+    @Autowired
+    public PageController(accountsRepository accRep, submitsRepository subRep){
+        this.accRep = accRep;
+        this.subRep = subRep;
+    }
+
     @GetMapping(path="/")
     public String index(Model model){
         model.addAttribute("appName", appName);
         return "index";
     }
 
-    accountsRepository a;
+    //TODO: Change this to cookie, and save user as cookie if we want user to see their page
+    @GetMapping("/profile/{username}")
+    public String profile(@PathVariable("username") String username, Model model) {
 
-    @GetMapping("/profile")
-    public String profile(Model model/*,
-                          @CookieValue("username") String usrNameCookie,
-                          @CookieValue("accType") String accType*/
-                         ) {
-
-        //model.addAttribute("username", usrNameCookie);
-        //model.addAttribute("accType", accType);
+        accounts account = accRep.findUser(username);
+        model.addAttribute("account", account);
         return "profile";
     }
 
+    //TODO: Pull from Submission or submits
     @GetMapping("/results")
     public String results(Model model) {
         //model.addAttribute("result", result);
         return "results";
     }
 
-    private List<Scoreboard> scoreList = new ArrayList<>();
-
+    //TODO: Is this right? Submission or submits?
     @GetMapping("/scoreboard")
     public String scoreboard(Model model){
-        model.addAttribute("scores", scoreList);
+        model.addAttribute("scores", subRep.findAll());
         return "scoreboard";
     }
 
--- caustic/src/main/java/com/ryan3r/caustic/controller/submitsController.java
+++ caustic/src/main/java/com/ryan3r/caustic/controller/submitsController.java
@@ -1,15 +1,17 @@
 package com.ryan3r.caustic.controller;
-import java.io.IOException;
 
 import com.ryan3r.caustic.repository.submitsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 @RestController
 public class submitsController {
@@ -19,7 +21,7 @@ public class submitsController {
 	@RequestMapping(method=RequestMethod.POST, value="/submit", consumes=MediaType.MULTIPART_FORM_DATA_VALUE)
 	public void uploadMapServer(@RequestParam("upload") MultipartFile file) throws IOException {
 		String idp = new String("IDPLACEHOLDER");
-		File f = new File("IDPLACEHOLDER","/mnt/submissions"); 
+		File f = new File("IDPLACEHOLDER","/mnt/submissions");
 		if(!f.exists())
 		{
 			f.mkdir();
